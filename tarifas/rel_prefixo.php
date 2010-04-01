050917b95afb79ebafae42d312e58bbd886aa663
rbozzetti <rbozzetti@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2010-04-01 18:12:07 +0000

    retorna prefixo do municipio (ajax)

---
+++ tarifas/rel_prefixo.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *  This file is part of SNEP.
+ *
+ *  SNEP is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SNEP is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with SNEP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once("../includes/verifica.php");
+require_once("../configs/config.php");
+
+$cidade = isset($_POST['cidade']) && $_POST['cidade']!= "" ? $_POST['cidade'] : display_error($LANG['msg_nostate'],true);
+$prefixo = Snep_Cnl::getPrefixo($cidade);
+
+print_r($prefixo);
+?>
