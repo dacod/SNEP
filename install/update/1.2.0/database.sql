779e2bbe1df8464d68c11ec72f7cbb3ab31a1366
rbozzetti <rbozzetti@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2010-07-06 22:33:19 +0000

    inclusao da tabela de alertas no sql de update

--- install/update/1.2.0/database.sql
+++ install/update/1.2.0/database.sql
@@ -1,3 +1,16 @@
 /* Criação do campo para senhas dos ramais. Independentes da senha do SIP/IAX2*/
 ALTER TABLE `peers` ADD `password` VARCHAR(12) NOT NULL;
 UPDATE `peers` SET `password`=`secret`;
+
+/* Tabela de Alertas de filas */
+CREATE TABLE `alertas` (
+  `recurso` VARCHAR( 20 ) NOT NULL ,
+  `tipo` VARCHAR( 10 ) NOT NULL ,
+  `tme` INT( 10 ) NOT NULL ,
+  `sla` INT( 10 ) NOT NULL ,
+  `item` VARCHAR( 20 ) NOT NULL ,
+  `alerta` VARCHAR( 255 ) NOT NULL ,
+  `destino` VARCHAR( 255 ) NOT NULL ,
+  `ativo` TINYINT( 1 ) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
