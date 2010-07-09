47262f925d126366c40100eb7d531e772470113d
rbozzetti <rbozzetti@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2010-07-09 14:49:57 +0000

    Correção do debug nao estava desabilitando o mesmo

--- configs/parametros.php
+++ configs/parametros.php
@@ -23,45 +23,72 @@
     gravar() ;
  }
  $config = Zend_Registry::get('config');
- $smarty->assign('OPCOES_TF',$tipos_tf) ;
- $smarty->assign('OPCOES_TIME',$tipos_time) ;
- $smarty->assign('OPCOES_CONF',$tipos_conference) ;
- $smarty->assign('record_app',strtolower($config->general->record->application));
- $smarty->assign('record_flags',$config->general->record->flags);
- 
- display_template("parametros.tpl",$smarty,$titulo) ;
+
+ $smarty->assign( 'OPCOES_TF',      $tipos_tf) ;
+ $smarty->assign( 'OPCOES_TIME',    $tipos_time) ;
+ $smarty->assign( 'OPCOES_CONF',    $tipos_conference) ;
+ $smarty->assign( 'record_mp3',     $config->system->record_mp3);
+ $smarty->assign( 'record_flags',   $config->general->record->flags);
+ $smarty->assign( 'record_app',     strtolower($config->general->record->application));
+ display_template("pbx/parametros.tpl", $smarty,$titulo) ;
     
 /*------------------------------------------------------------------------------
  Funcao gravar - Converte e grava o arquivo
  ------------------------------------------------------------------------------*/
 function gravar()  {
-   global $db, $LANG; 
+
+   global $db, $LANG;
+
    $config_file = Zend_Registry::get('configFile');
    $config_tmp = "/tmp/setup.conf"; 
    $erro = FALSE ;
+
    if (count($_POST['alterar']) > 0 ) {
+
       foreach ( $_POST["alterar"] as $chave => $option ) {
+
          $value = str_replace(",","\,", $_POST["new_$option"] );
          $option_sed = $option == "record_flags"? "record\.flags" : $option;
          $option_sed = $option == "record_app"? "record\.application" : $option;
 
          // Pesquisa e faz a troca do valor da variavel - salva em arquivo temporario
-         if($option == "debug") {
+         if($option == "new_debug") {
             $comando = "sed \"s,^$option_sed.*=.*,$option_sed = $value\", < \"$config_file\" > \"$config_tmp\"";
+
          }
+         elseif($option == "record_mp3") {             
+            if($value == 'true') {
+                $comando = "sed \"s,^$option_sed.*=.*,$option_sed=true\", < \"$config_file\" > \"$config_tmp\"";
+            }else{
+                $comando = "sed \"s,^$option_sed.*=.*,$option_sed=false\", < \"$config_file\" > \"$config_tmp\"";
+            }
+         }elseif($option == "record_compact") {
+            if($value == 'true') {
+                $comando = "sed \"s,^$option_sed.*=.*,$option_sed=true\", < \"$config_file\" > \"$config_tmp\"";
+            }else{
+                $comando = "sed \"s,^$option_sed.*=.*,$option_sed=false\", < \"$config_file\" > \"$config_tmp\"";
+            }
+         }
+
          else {
             $comando = 'sed "s,^'.$option_sed.'.*=.*,'.$option_sed.' = \"'.$value.'\"'.'", < "'.$config_file.'" > "'.$config_tmp.'"';
          }
+
          if (executacmd($comando,$LANG['msg_err_sed'])) {
             // Ajusta permissoes do arquivo temporario
-            $comando = 'mv '.$config_tmp.' '.$config_file ;
+            $comando = 'mv '. $config_tmp .' '. $config_file ;
+            
             if (executacmd($comando,$LANG['msg_err_move'])) {
                $erro = TRUE ;
             }
          }
+
       } // Fim do foreach
-      if (!$erro)
-         display_error($LANG['msg_ok_config'],true) ;
+
+      if (!$erro) {
+          display_error($LANG['msg_ok_config'],true) ;
+      }
+         
    } // Fim do If
-  //echo "<meta http-equiv='refresh' content='0;url=../configs/parametros.php'>\n" ;
+  echo "<meta http-equiv='refresh' content='0;url=../configs/parametros.php'>\n" ;
 }
