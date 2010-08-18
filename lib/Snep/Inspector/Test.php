09f2020d2562c8d6509023689e8f39c1876973e7
rbozzetti <rbozzetti@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2010-08-18 13:16:50 +0000

    Classe abstrata do Inspector, para inspeção e verificação do sistema

---
+++ lib/Snep/Inspector/Test.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ *  This file is part of SNEP.
+ *
+ *  SNEP is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  SNEP is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with SNEP.  If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+ */
+
+/**
+ * Classe abstrata que extende-se aos /inspectors/.
+ *
+ * @see Snep_Inspector_Test
+ *
+ * @category  Snep
+ * @package   Snep
+ * @copyright Copyright (c) 2010 OpenS Tecnologia
+ * @author    Rafael Pereira Bozzetti <rafael@opens.com.br>
+ *
+ */
+
+abstract class Snep_Inspector_Test {
+
+    /**
+     * Classe abstrata sobrescritas pelas classes do /inspectors/
+     */
+    abstract public function getTests();
+
+    /**
+     * Retorna usuário do serviço Httpd
+     * @return Array
+     */
+    public function getHttpUser() {
+        $user['name'] = exec('whoami');
+        $user['id'] = exec('id -u '.$user['name'] );
+        return $user;
+    }
+}
