10de955f2340d4d7f3acfcc217458283eac76441
rbozzetti <rbozzetti@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2010-08-26 14:29:44 +0000

    Controlador da rotina de inspecao do snep

---
+++ default/controllers/InspectorController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *  This file is part of SNEP.
+ *
+ *  SNEP is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SNEP is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with SNEP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+class InspectorController extends Zend_Controller_Action {
+
+    public function indexAction() {
+
+        $this->view->breadcrumb = $this->view->translate("Configurações » Diagnóstico do Sistema");
+
+        // cria objeto da classe Snep_Inspector
+        $obj = new Snep_Inspector();
+
+        // Pega array de informacoes gerados no construtor
+        $inspect = $obj->getInspects();
+
+        $this->view->inspect = $inspect;
+    }
+}
+
+
