633c5e8886ff24390b15299c656f63a59f1509fd
rbozzetti <rbozzetti@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2010-09-15 13:29:23 +0000

    Redirecionamento para instalador, se for o caso..

--- default/controllers/IndexController.php
+++ default/controllers/IndexController.php
@@ -5,6 +5,14 @@ require_once 'Zend/Controller/Action.php';
 class IndexController extends Zend_Controller_Action {
     public function indexAction() {
         // Direcionando para o "snep antigo"
-        $this->_redirect("../src/sistema.php");
+        $config = Zend_Registry::get('config');
+
+        if( trim ( $config->ambiente->db->host ) == "" ) {            
+            $this->_redirect("/installer/");
+
+        }else{            
+            $this->_redirect("../src/sistema.php");
+        }
+        
     }
 }
