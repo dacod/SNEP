7ff431d216ffa90f3f34de7dba3db9ba5737a83f
rbozzetti <rbozzetti@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2010-12-09 13:40:38 +0000

    Inclusão do método setButtom()

--- lib/Snep/Form.php
+++ lib/Snep/Form.php
@@ -14,7 +14,28 @@ class Snep_Form extends Zend_Form {
             array('Label', array('tag' => 'dt')),
             array(array('elementDiv' => 'HtmlTag'), array('tag' => 'div', 'class'=>'form_element'))
         ));
+
     }
+
+    public function setButtom() {
+
+        $submit = new Zend_Form_Element_Submit("submit", array("label" => "Salvar"));
+        $submit->removeDecorator('DtDdWrapper');
+        $submit->addDecorator('HtmlTag', array('tag' => 'li'));
+        $submit->addDecorator('HtmlTag', array('tag' => 'div', 'class' => 'menus', 'openOnly' => true, 'placement' => Zend_Form_Decorator_Abstract::PREPEND ));
+
+        $this->addElement($submit);
+
+        $back = new Zend_Form_Element_Button("buttom", array("label" => "Cancelar" ));
+        $back->setAttrib("onclick", "location.href='javascript:history.back();'");
+        $back->removeDecorator('DtDdWrapper');
+        $back->addDecorator('HtmlTag', array('tag' => 'li'));
+        $back->addDecorator('HtmlTag', array('tag' => 'div', 'class' => 'menus', 'closeOnly' => true, 'placement' => Zend_Form_Decorator_Abstract::APPEND));
+
+        $this->addElement($back);
+       
+    }
+
 
 }
 
