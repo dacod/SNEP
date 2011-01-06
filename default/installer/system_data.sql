dfd861a9d6d4d415900191b7e834147d3af0d4b0
lucasseid <lucasseid@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2011-01-06 18:07:55 +0000

    Adição de tipos de Alias de Expressão Regular, como solicitado no bug #0000064

--- default/installer/system_data.sql
+++ default/installer/system_data.sql
@@ -17,13 +17,22 @@
 
 INSERT INTO `expr_alias` VALUES (1,'Fixo Local');
 INSERT INTO `expr_alias` VALUES (2,'Celular Local - VC1');
-INSERT INTO `expr_alias` VALUES (3,'Fixo DDD');
+INSERT INTO `expr_alias` VALUES (3,'Fixo LDN');
 INSERT INTO `expr_alias` VALUES (4,'Celular Interurbano - VC2/VC3');
+INSERT INTO `expr_alias` VALUES (5,'Fixo/Celular LDI');
+INSERT INTO `expr_alias` VALUES (6,'Serviços Especiais');
 
-INSERT INTO `expr_alias_expression` VALUES (1,'[2-6]XXXXXXX');
-INSERT INTO `expr_alias_expression` VALUES (2,'[7-9]XXXXXXX');
+INSERT INTO `expr_alias_expression` VALUES (1,'[2-5]XXXXXXX');
+INSERT INTO `expr_alias_expression` VALUES (2,'[6-9]XXXXXXX');
+INSERT INTO `expr_alias_expression` VALUES (3,'0XX|XX[2-5]XXXXXXX');
 INSERT INTO `expr_alias_expression` VALUES (3,'0|XX[2-5]XXXXXXX');
+INSERT INTO `expr_alias_expression` VALUES (4,'0XX|XX[6-9]XXXXXXX');
 INSERT INTO `expr_alias_expression` VALUES (4,'0|XX[6-9]XXXXXXX');
+INSERT INTO `expr_alias_expression` VALUES (5,'00|X.');
+INSERT INTO `expr_alias_expression` VALUES (5,'00XX|X.');
+INSERT INTO `expr_alias_expression` VALUES (6,'0[3-9]00X.');
+INSERT INTO `expr_alias_expression` VALUES (6,'1XX');
+INSERT INTO `expr_alias_expression` VALUES (6,'10XXX');
 
 INSERT INTO groups VALUES ('all',null);
 INSERT INTO groups VALUES ('admin','all');
