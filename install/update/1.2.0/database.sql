4ecba1e858116f86d79e75dc87532d3dfe12c0d5
guaxinim <guaxinim@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2011-01-17 13:10:59 +0000

    Corrigindo scripts de udate

--- install/update/1.2.0/database.sql
+++ install/update/1.2.0/database.sql
@@ -1,6 +1,3 @@
-INSERT INTO `rotinas` VALUES (114, 'Diagnóstico do Sistema');
-INSERT INTO `permissoes` VALUES (114, 1, 'S');
-
 /* Criação do campo para senhas dos ramais. Independentes da senha do SIP/IAX2*/
 ALTER TABLE `peers` ADD `password` VARCHAR(12) NOT NULL;
 UPDATE `peers` SET `password`=`secret`;
