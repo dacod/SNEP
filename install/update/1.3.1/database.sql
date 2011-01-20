ae4b6b0cc05821f8c147dfbe3d6e6668980a62f2
guaxinim <guaxinim@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2011-01-20 00:49:30 +0000

    Adicionando informações de banco faltantes

--- install/update/1.3.1/database.sql
+++ install/update/1.3.1/database.sql
@@ -1,2 +1,36 @@
 update groups set name='administrator' where name = 'admin';
 ALTER TABLE  `trunks` ADD  `domain` VARCHAR( 250 ) NOT NULL
+
+CREATE TABLE IF NOT EXISTS ars_operadora (
+    `id` integer primary key,
+    `name` varchar(30) not null
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS ars_estado (
+    `cod` char(2) primary key,
+    `name` varchar(30) not null
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS ars_cidade (
+    `id` integer primary key auto_increment,
+    `name` varchar(30) not null
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS ars_ddd (
+    `cod` char(2),
+    `estado` char(2),
+    `cidade` integer,
+    primary key (`cod`,`estado`,`cidade`),
+    foreign key (`estado`) references ars_estado(`cod`) on update cascade on delete restrict,
+    foreign key (`cidade`) references ars_cidade(`id`) on update cascade on delete restrict
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS ars_prefixo (
+    `prefixo` integer,
+    `cidade` integer,
+    `operadora` integer,
+    primary key (`prefixo`,`cidade`,`operadora`),
+    foreign key (`operadora`) references ars_operadora(`id`) on update cascade on delete restrict,
+    foreign key (`cidade`) references ars_cidade(`id`) on update cascade on delete restrict
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
