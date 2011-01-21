a5ee559f750f3e7874b9e5b3bbdaa2965d12bb1e
guaxinim <guaxinim@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2011-01-21 01:31:04 +0000

    Ajustando layout da rotina de atualização de CNL

--- lib/Snep/Form.php
+++ lib/Snep/Form.php
@@ -16,5 +16,27 @@ class Snep_Form extends Zend_Form {
         ));
     }
 
+    public function setButtom() {
+        $this->setButton();
+    }
+
+    public function setButton() {
+        $submit = new Zend_Form_Element_Submit("submit", array("label" => "Salvar"));
+        $submit->removeDecorator('DtDdWrapper');
+        $submit->addDecorator('HtmlTag', array('tag' => 'li'));
+        $submit->addDecorator('HtmlTag', array('tag' => 'div', 'class' => 'menus', 'openOnly' => true, 'placement' => Zend_Form_Decorator_Abstract::PREPEND ));
+
+        $this->addElement($submit);
+
+        $back = new Zend_Form_Element_Button("buttom", array("label" => "Cancelar" ));
+        $back->setAttrib("onclick", "location.href='javascript:history.back();'");
+        $back->removeDecorator('DtDdWrapper');
+        $back->addDecorator('HtmlTag', array('tag' => 'li'));
+        $back->addDecorator('HtmlTag', array('tag' => 'div', 'class' => 'menus', 'closeOnly' => true, 'placement' => Zend_Form_Decorator_Abstract::APPEND));
+
+        $this->addElement($back);
+    }
+
+
 }
 
