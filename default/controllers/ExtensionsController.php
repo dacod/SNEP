94cad898b4c0d7616d45d0c86d4be0d515cc938d
guaxinim <guaxinim@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2011-01-21 01:10:37 +0000

    Atualizações de CSS e amostra de filtro para correções gerais de CSS e reaproveitamento de código.

--- default/controllers/ExtensionsController.php
+++ default/controllers/ExtensionsController.php
@@ -15,89 +15,63 @@
  *  You should have received a copy of the GNU General Public License
  *  along with SNEP.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-
-
 class ExtensionsController extends Zend_Controller_Action {
 
     public function indexAction() {
-       
+
         $this->view->breadcrumb = $this->view->translate("Cadastro » Ramais");
-        
+
         $db = Zend_Registry::get('db');
-        $select = $db->select()->from("peers",array(
-                "id" => "id",
-                "exten" => "name",
-                "name" => "callerid",
-                "channel" => "canal",
-                "group"
-        ));
+        $select = $db->select()->from("peers", array(
+                    "id" => "id",
+                    "exten" => "name",
+                    "name" => "callerid",
+                    "channel" => "canal",
+                    "group"
+                ));
         $select->where("peer_type='R'");
 
-        if( $this->_request->getPost('filtro') ) {
-            $field = mysql_escape_string( $this->_request->getPost('campo') );
-            $query = mysql_escape_string( $this->_request->getPost('filtro') );
+        if ($this->_request->getPost('filtro')) {
+            $field = mysql_escape_string($this->_request->getPost('campo'));
+            $query = mysql_escape_string($this->_request->getPost('filtro'));
             $select->where("`$field` like '%$query%'");
         }
 
         $page = $this->_request->getParam('page');
-        $this->view->page = ( isset($page) && is_numeric($page)  ? $page : 1 );
+        $this->view->page = ( isset($page) && is_numeric($page) ? $page : 1 );
 
         $this->view->filtro = $this->_request->getParam('filtro');
-        
+
         $paginatorAdapter = new Zend_Paginator_Adapter_DbSelect($select);
         $paginator = new Zend_Paginator($paginatorAdapter);
 
-        $paginator->setCurrentPageNumber( $this->view->page );
+        $paginator->setCurrentPageNumber($this->view->page);
         $paginator->setItemCountPerPage(Zend_Registry::get('config')->ambiente->linelimit);
 
         $this->view->extensions = $paginator;
         $this->view->pages = $paginator->getPages();
         $this->view->PAGE_URL = "/snep/index.php/extensions/index/";
 
-        $opcoes = array("name"     => $this->view->translate("Ramal"),
-                        "callerid" => $this->view->translate("Nome"),
-                        "group"    => $this->view->translate("Grupo")
+        $options = array("name" => $this->view->translate("Ramal"),
+            "callerid" => $this->view->translate("Nome"),
+            "group" => $this->view->translate("Grupo")
         );
 
         // Formulário de filtro.
-        $config_file = "./default/forms/filter.xml";
-        $config = new Zend_Config_Xml($config_file, null, true);
-
-        $filter = new Zend_Form( $config->filter );
-        $filter->setAction( $this->getFrontController()->getBaseUrl() . '/extensions/index' );
-
-        $filter_value = $filter->getElement('filtro');
-        $filter_value->setValue( $this->_request->getPost('filtro') );
-
-        $submit = new Zend_Form_Element_Submit("submit", array("label" => $this->view->translate("Procurar")));
-        $submit->removeDecorator('DtDdWrapper');
-        $submit->addDecorator('HtmlTag', array('tag' => 'dd'));
-
-        // Botão Lista Completa
-        $reset = new Zend_Form_Element_Button("buttom", array("label" => $this->view->translate("Lista Completa") ) );
-        $reset->setAttrib("onclick", "location.href='{$this->getFrontController()->getBaseUrl()}/{$this->getRequest()->getControllerName()}/index/page/$page'");
-        $reset->removeDecorator('DtDdWrapper');
-        $reset->addDecorator('HtmlTag', array('tag' => 'dd'));    
-
-        // Define elementos do 'campo' select
-        $campo = $filter->getElement('campo');
-        $campo->setMultiOptions( $opcoes );
-
-        $filtro = $filter->getElement('filtro');
-        $filtro->setValue( $this->_request->getParam('filtro') );
-
-        $filter->addElement( $submit);
-        $filter->addElement( $reset );
+        $filter = new Snep_Form_Filter();
+        $filter->setAction($this->getFrontController()->getBaseUrl() . '/extensions/index');
+        $filter->setValue($this->_request->getPost('campo'));
+        $filter->setFieldOptions($options);
+        $filter->setFieldValue($this->_request->getParam('filtro'));
+        $filter->setResetUrl("{$this->getFrontController()->getBaseUrl()}/{$this->getRequest()->getControllerName()}/index/page/$page");
 
         $this->view->form_filter = $filter;
-        $this->view->filter = array( array("url" => "/snep/src/extensions.php?action=multiadd",
-                                           "display" => "Incluir Ramais",
-                                           "css" => "includes"),
-                                     array("url" => "/snep/src/extensions.php?action=add",
-                                           "display" => "Incluir Ramal",
-                                           "css" => "include") );
-        
+        $this->view->filter = array(array("url" => "/snep/src/extensions.php?action=multiadd",
+                "display" => "Incluir Ramais",
+                "css" => "includes"),
+            array("url" => "/snep/src/extensions.php?action=add",
+                "display" => "Incluir Ramal",
+                "css" => "include"));
     }
 
     /**
@@ -128,12 +102,11 @@ class ExtensionsController extends Zend_Controller_Action {
 
         // Código legado espera id da table peers e não numero do ramal.
         $db = Zend_Registry::get("db");
-        $select = $db->select()->from("peers",array("id"))->where("name='$exten' AND peer_type='R'");
+        $select = $db->select()->from("peers", array("id"))->where("name='$exten' AND peer_type='R'");
         $result = $select->query()->fetchObject();
 
         $this->__redirect("./ramais.php?acao=alterar&id=$result->id");
     }
-
 
     public function deleteAction() {
 
@@ -142,8 +115,8 @@ class ExtensionsController extends Zend_Controller_Action {
 
         $id = isset($_GET['id']) ? $_GET['id'] : false;
         if (!$id) {
-            display_error($LANG['msg_notselect'],true);
-            exit ;
+            display_error($LANG['msg_notselect'], true);
+            exit;
         }
 
         // Fazendo procura por referencia a esse ramal em regras de negócio.
@@ -153,15 +126,15 @@ class ExtensionsController extends Zend_Controller_Action {
         $rules_query = "SELECT rule.id, rule.desc FROM regras_negocio as rule, regras_negocio_actions_config as rconf WHERE (rconf.regra_id = rule.id AND rconf.value = '$id')";
         $regras = array_merge($regras, $db->query($rules_query)->fetchAll());
 
-        if(count($regras) > 0) {
-            $msg = $LANG['extension_conflict_in_rules'].":<br />\n";
+        if (count($regras) > 0) {
+            $msg = $LANG['extension_conflict_in_rules'] . ":<br />\n";
             foreach ($regras as $regra) {
                 $msg .= $regra['id'] . " - " . $regra['desc'] . "<br />\n";
             }
-            display_error($msg,true);
+            display_error($msg, true);
             exit(1);
         }
-        $sql = "DELETE FROM peers WHERE name='".$id."'";
+        $sql = "DELETE FROM peers WHERE name='" . $id . "'";
 
         $db->beginTransaction();
 
@@ -176,12 +149,10 @@ class ExtensionsController extends Zend_Controller_Action {
             grava_conf();
         } catch (PDOException $e) {
             $db->rollBack();
-            display_error($LANG['error'].$e->getMessage(),true) ;
+            display_error($LANG['error'] . $e->getMessage(), true);
         }
-        
+
         $this->_redirect("./default/extensions/");
     }
-
-
 
 }
--- default/controllers/TrunksController.php
+++ default/controllers/TrunksController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *  This file is part of SNEP.
  *
@@ -15,97 +16,54 @@
  *  You should have received a copy of the GNU General Public License
  *  along with SNEP.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-
-
 class TrunksController extends Zend_Controller_Action {
 
     public function indexAction() {
         $this->view->breadcrumb = $this->view->translate("Cadastro » Troncos");
-     
+
         $db = Zend_Registry::get('db');
 
         $select = $db->select()
-                ->from("trunks", array("id", "callerid", "name", "type", "trunktype"));
+                        ->from("trunks", array("id", "callerid", "name", "type", "trunktype"));
 
-        if( $this->_request->getPost('filtro') ) {
-            $field = mysql_escape_string( $this->_request->getPost('campo') );
-            $query = mysql_escape_string( $this->_request->getPost('filtro') );
+        if ($this->_request->getPost('filtro')) {
+            $field = mysql_escape_string($this->_request->getPost('campo'));
+            $query = mysql_escape_string($this->_request->getPost('filtro'));
             $select->where("`$field` like '%$query%'");
         }
 
         $page = $this->_request->getParam('page');
-        $this->view->page = ( isset($page) && is_numeric($page)  ? $page : 1 );
+        $this->view->page = ( isset($page) && is_numeric($page) ? $page : 1 );
 
         $this->view->filtro = $this->_request->getParam('filtro');
-        
+
         $paginatorAdapter = new Zend_Paginator_Adapter_DbSelect($select);
         $paginator = new Zend_Paginator($paginatorAdapter);
 
-        $paginator->setCurrentPageNumber( $this->view->page );
+        $paginator->setCurrentPageNumber($this->view->page);
         $paginator->setItemCountPerPage(Zend_Registry::get('config')->ambiente->linelimit);
 
         $this->view->trunks = $paginator;
         $this->view->pages = $paginator->getPages();
         $this->view->PAGE_URL = "/snep/index.php/trunks/index/";
 
-        $opcoes = array("callerid" => $this->view->translate("Nome"),
-                        "type" => $this->view->translate("Tecnologia") );
+        $opcoes = array("name" => $this->view->translate("Código"),
+            "callerid" => $this->view->translate("Nome")
+        );
 
         // Formulário de filtro.
-        $config_file = "./default/forms/filter.xml";
-        $config = new Zend_Config_Xml($config_file, null, true);
+        $filter = new Snep_Form_Filter();
+        $filter->setAction($this->getFrontController()->getBaseUrl() . '/' . $this->getRequest()->getControllerName() . '/index');
+        $filter->setValue($this->_request->getPost('campo'));
+        $filter->setFieldOptions($opcoes);
+        $filter->setFieldValue($this->_request->getPost('filtro'));
+        $filter->setResetUrl("{$this->getFrontController()->getBaseUrl()}/{$this->getRequest()->getControllerName()}/index/page/$page");
 
-        $form = new Zend_Form();
-        $form->setAction( $this->getFrontController()->getBaseUrl() . '/trunks/index' );
-
-        $filter = new Zend_Form( $config->filter );
-
-        $filter_value = $filter->getElement('filtro');
-        $filter_value->setValue( $this->_request->getPost('filtro') );
-
-        $submit = new Zend_Form_Element_Submit("submit", array("label" => $this->view->translate("Procurar")));
-        $submit->removeDecorator('DtDdWrapper');
-        $submit->addDecorator('HtmlTag', array('tag' => 'dd'));
-
-        // Botão Lista Completa
-        $reset = new Zend_Form_Element_Button("buttom", array("label" => $this->view->translate("Lista Completa") ) );
-        $reset->setAttrib("onclick", "location.href='{$this->getFrontController()->getBaseUrl()}/{$this->getRequest()->getControllerName()}/index/page/$page'");
-        $reset->removeDecorator('DtDdWrapper');
-        $reset->addDecorator('HtmlTag', array('tag' => 'dd'));    
-
-        // Define elementos do 'campo' select
-        $campo = $filter->getElement('campo');
-        $campo->setMultiOptions( $opcoes );
-
-        $filtro = $filter->getElement('filtro');
-        $filtro->setValue( $this->_request->getParam('filtro') );
-
-        $filter->addElement( $submit);
-        $filter->addElement( $reset );
-
-        $form->addSubForm($filter, "filter");        
-        $this->view->form_filter = $form;
-        $this->view->filter = array( array("url" => "/snep/src/troncos.php",
-                                           "display" => $this->view->translate("Incluir Tronco"),
-                                           "css" => "include"),
-                                   );
-        
+        $this->view->form_filter = $filter;
+        $this->view->filter = array(array("url" => "/snep/src/troncos.php",
+                "display" => $this->view->translate("Incluir Tronco"),
+                "css" => "include"),
+        );
     }
-
-
-    public function addAction() {
-        
-    }
-
-    public function editAction() {
-
-    }
-
-
-    public function deleteAction() {
-
-    }
-
 
 }
