90584fc083c5105f75885534ae6a4ce929ac719b
esantana <esantana@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2011-03-21 18:16:25 +0000

    cadastro de grupo de captura

---
+++ lib/Snep/PickupGroups/Manager.php
@@ -0,0 +1,135 @@
+<?php
+/**
+ *  This file is part of SNEP.
+ *
+ *  SNEP is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  SNEP is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with SNEP.  If not, see <http://www.gnu.org/licenses/lgpl.txt>.
+ */
+
+/**
+ *  Class that  controls  the  persistence  in  database  of business rules
+ * the Snep.
+ *
+ * Note about  persistence: The  persistence  control  is  done  in  the  SNEP
+ * separate classes. Not in the constructor of the class model as is seen in other
+ * Frameworks and architectures. The reason is that if a change in
+ * how it is made ​​the persistence of these objects need not be the same
+ * changed. This increases the compactness with legacy code and facilitates
+ * migration of code between versions.
+ * ~henrique
+ *
+ * @category  Snep
+ * @package   Snep
+ * @copyright Copyright (c) 2010 OpenS Tecnologia
+ * @author Henrique Grolli Bassotto
+ */
+class Snep_PickupGroups_Manager {
+
+    private function __construct() { /* Protegendo métodos dinâmicos */ }
+    private function __destruct() { /* Protegendo métodos dinâmicos */ }
+    private function __clone() { /* Protegendo métodos dinâmicos */ }
+
+    /**
+     * Remove a pickup group from the database based on his  ID.
+     *
+     * @param int $id
+     */
+    public static function delete($id) {
+
+        $db = Zend_Registry::get('db');
+        $db->delete("grupos","cod_grupo='{$id}'");
+    }
+
+    /**
+     * Return a pickup group from the database based on his  ID.
+     *
+     * @param int $id
+     */
+    public static function get ($id) {
+
+        $db = Zend_Registry::get('db');
+        $select = $db->select()
+                ->from ('grupos')
+                ->where ("cod_grupo = '$id'");
+
+        $stmt = $db->query($select);
+        $registros = $stmt->fetch();
+
+        return $registros;
+    }
+
+    public static function getAll() {
+
+        $db = Zend_Registry::get('db');
+        $select = $db->select()
+                ->from ('grupos');
+
+        $stmt = $db->query ($select);
+        $row = $stmt->fetchAll();
+
+        $pickupGroups = array();
+        foreach ($row as $pickupGroup) {
+            $pickupGroups[$pickupGroup['cod_grupo']] = $pickupGroup['nome'];
+        }
+        return $pickupGroups;
+    }
+
+    public static function getFilter ($field, $query) {
+
+        $db = Zend_Registry::get('db');
+        $select = $db->select()
+                ->from ('grupos');
+
+        if (!is_null($query)) {
+                $select->where ("$field like '%$query%'");
+        }
+
+        return $select;
+    }
+
+    public static function add($pickupGroup) {
+
+        $db = Zend_Registry::get('db');
+        $db->beginTransaction();
+
+        try {
+            $db->insert('grupos', $pickupGroup);
+            $db->commit();
+            return true;
+        }
+        catch(Exception $e) {
+            $db->rollBack();
+            return $e;
+        }
+    }
+
+    public static function  edit($pickupGroup) {
+
+        $db = Zend_Registry::get('db');
+        $db->beginTransaction();
+        $value = array ('nome' => $pickupGroup['name']);
+
+        try {
+
+            $db->update ('grupos', $value, 'cod_grupo ='.$pickupGroup['id']);
+            $db->commit();
+            return true;
+        }
+        catch(Exception $e) {
+
+            $db->rollBack();
+            return $e;
+        }
+    }
+}
+?>
