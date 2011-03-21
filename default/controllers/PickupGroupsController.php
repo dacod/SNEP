306038cb1d3f7c133725db691ffa076b93093435
esantana <esantana@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2011-03-21 18:15:21 +0000

    cadastro de grupos de captura

--- default/controllers/PickupGroupsController.php
+++ default/controllers/PickupGroupsController.php
@@ -16,13 +16,32 @@
  *  You should have received a copy of the GNU General Public License
  *  along with SNEP.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/**
+ * controller  pickup groups.
+ */
 class PickupGroupsController extends Zend_Controller_Action {
+
+    /**
+     * Add/Edit form for routes
+     *
+     * @var Zend_Form
+     */
+    protected $form;
+    /**
+     * Sub-form for Action Rules
+     *
+     * @var array
+     */
+    protected $forms;
 
     public function indexAction() {
 
         $this->view->breadcrumb = $this->view->translate("Cadastro » Grupos de Captura");
 
         $db = Zend_Registry::get('db');
+
+
         $select = $db->select()
                         ->from("grupos");
 
@@ -43,7 +62,8 @@ class PickupGroupsController extends Zend_Controller_Action {
 
         $this->view->pickupgroups = $paginator;
         $this->view->pages = $paginator->getPages();
-        $this->view->PAGE_URL = "/snep/index.php/{$this->getRequest()->getControllerName()}/index/";
+        $this->view->URL = "{$this->getFrontController()->getBaseUrl()}/{$this->getRequest()->getControllerName()}/";
+        $this->view->PAGE_URL = "{$this->getFrontController()->getBaseUrl()}/{$this->getRequest()->getControllerName()}/index/";
 
         $opcoes = array("cod_grupo" => $this->view->translate("Código"),
             "nome" => $this->view->translate("Nome"));
@@ -57,10 +77,90 @@ class PickupGroupsController extends Zend_Controller_Action {
         $filter->setResetUrl("{$this->getFrontController()->getBaseUrl()}/{$this->getRequest()->getControllerName()}/index/page/$page");
 
         $this->view->form_filter = $filter;
-        $this->view->filter = array(array("url" => "/snep/src/grupos.php",
+        $this->view->filter = array(array("url" => "{$this->getFrontController()->getBaseUrl()}/{$this->getRequest()->getControllerName()}/add",
                 "display" => $this->view->translate("Incluir Grupos de Captura"),
                 "css" => "include")
         );
+
+    }
+
+    public function addAction() {
+
+        $this->view->breadcrumb = $this->view->translate("Cadastro » Grupos de Captura » Incluir Grupos de Captura");
+        $form_xml = new Zend_Config_Xml("default/forms/pickupGroup.xml");
+        $form = new Snep_Form($form_xml->general);
+        $form->setAction($this->getFrontController()->getBaseUrl() . '/' . $this->getRequest()->getControllerName().'/add');
+        $name = $form->getElement('name')->setLabel($this->view->translate("Nome"));        
+
+        if ($this->getRequest()->getPost()) {
+
+            $form_isValid = $form->isValid($_POST);
+            $dados = $this->_request->getParams();
+
+            if ($form_isValid) {
+                $pickupgroup = array('nome' => $dados['name']);
+
+                $this->view->group = Snep_PickupGroups_Manager::add($pickupgroup);
+
+                $this->_redirect("/".$this->getRequest()->getControllerName()."/");
+            }
+        }
+
+        $form->setButtom();
+        $this->view->form = $form;
+
+    }
+
+    public function editAction() {
+
+        $this->view->breadcrumb = $this->view->translate("Cadastro » Grupos de Captura » Alterar Grupos de Captura");
+
+        $id = $this->_request->getParam('id');
+        $pickupgroup = Snep_PickupGroups_Manager::get($id);
+
+        $form_xml = new Zend_Config_Xml("default/forms/pickupGroup.xml");
+        $form = new Snep_Form($form_xml->general);
+        $form->setAction($this->getFrontController()->getBaseUrl() . '/' . $this->getRequest()->getControllerName().'/edit');
+
+        $name = $form->getElement('name')->setValue($pickupgroup['nome']);
+        $id = $form->getElement('id')->setValue($pickupgroup['cod_grupo']);
+        $name = $form->getElement('name')->setLabel($this->view->translate("Nome"));
+
+        $form->setButtom();
+
+        if ($this->_request->getPost()) {
+
+            $form_isValid = $form->isValid($_POST);
+            $dados = $this->_request->getParams();
+
+            if ($form_isValid) {
+
+                $pickupgroup = array('id' => $dados['id'],
+                    'name' => $dados['name']
+                );
+
+                $this->view->Group = Snep_PickupGroups_Manager::edit($pickupgroup);
+                $this->_redirect($this->getRequest()->getControllerName());
+            }
+        }
+        $this->view->form = $form;
+
+    }
+
+    public function deleteAction() {
+
+        $id = mysql_escape_string($this->getRequest()->getParam('id'));
+
+        try {
+            $pickugroups = Snep_PickupGroups_Manager::get($id);
+        }
+        catch(PBX_Exception_NotFound $ex) {
+            throw new Zend_Controller_Action_Exception('Page not found.', 404);
+        }
+
+        Snep_PickupGroups_Manager::delete($id);
+
+        $this->_redirect($this->getRequest()->getControllerName());
     }
 
 }
