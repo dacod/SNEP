96b5b70ad3e07595e0b477c233260baf6cf026ce
guaxinim <guaxinim@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2011-04-17 15:26:32 +0000

    HTML e CSS da Tela de status do sistema.

--- default/controllers/IndexController.php
+++ default/controllers/IndexController.php
@@ -4,6 +4,8 @@ require_once 'Zend/Controller/Action.php';
 
 class IndexController extends Zend_Controller_Action {
     public function indexAction() {
+        $this->view->breadcrumb = $this->view->translate("Welcome to Snep PBX version %s", SNEP_VERSION);
+
         // Direcionando para o "snep antigo"
         $config = Zend_Registry::get('config');
 
