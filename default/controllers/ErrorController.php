3b6419bc085b60bce8dc8c3aef26f7622094a4d9
guaxinim <guaxinim@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2011-05-12 14:32:27 +0000

    Ajustando telas de erro

--- default/controllers/ErrorController.php
+++ default/controllers/ErrorController.php
@@ -14,20 +14,26 @@ class ErrorController extends Zend_Controller_Action {
         switch ($errors->type) {
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
-
                 // 404 error -- controller or action not found
                 $this->getResponse()->setHttpResponseCode(404);
-                $this->view->message = 'A pagina a qual você está procurando não foi encontrada.';
-                $this->view->title = '404 - Não Encontrado';
+                $this->view->code = 404;
+                $this->view->message = $this->view->translate("The page you are looking for was not found or does not exists.");
+                $this->view->title = $this->view->translate("Not Found");
                 break;
             default:
                 // application error
                 $this->getResponse()->setHttpResponseCode(500);
-                $this->view->title = '500 - Erro Interno';
+                $this->view->code = 500;
+                $this->view->title = $this->view->translate("Internal Error");
                 $this->view->sidebar = false;
-                $this->view->message = 'Por favor, contate o suporte o administrador do sistema.';
+                $this->view->message = $this->view->translate("Some internal error occured while processing your request. Please contact the system administrator and report this incident.");
                 break;
         }
+
+        $this->view->breadcrumb = Snep_Breadcrumb::renderPath(array(
+            $this->view->translate("Error"),
+            $this->view->title
+        ));
 
         $this->view->exception = $errors->exception;
         $this->view->request   = $errors->request;
