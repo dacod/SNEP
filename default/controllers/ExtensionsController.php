39a3293df1ec1029ac36c1aa279f247b5fb50415
lucasseid <lucasseid@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2011-05-18 13:22:08 +0000

    Resolução problemas Edição de ramais

--- default/controllers/ExtensionsController.php
+++ default/controllers/ExtensionsController.php
@@ -130,7 +130,13 @@ class ExtensionsController extends Zend_Controller_Action {
 
             if ($this->view->form->isValid($_POST)) {
                 $postData = $this->_request->getParams();
-
+                
+                if (key_exists('virtual', $postData)){
+                    if (!key_exists('virtual', $postData['virtual'])){
+                    $this->view->error = "There's no trunks registered on the system. Try a different technology";
+                    $this->view->form->valid(false);
+                    }
+                }
                 $ret = $this->execAdd($postData);
 
                 if (!is_string($ret)) {
@@ -548,7 +554,7 @@ class ExtensionsController extends Zend_Controller_Action {
             $subFormVirtual = new Snep_Form_SubForm(null, $form_xml->virtual, "virtual");
             if(PBX_Trunks::getAll() == null){
                 $subFormVirtual->removeElement('virtual');
-                $subFormVirtual->addElement(new Snep_Form_Element_Html("extensions/trunk_error.phtml", "err", false, null, "virtual"));
+                $subFormVirtual->addElement(new Snep_Form_Element_Html("extensions/trunk_error.phtml", "err", false, null, "error"));
             }
             $form->addSubForm($subFormVirtual, "virtual");
             $subFormKhomp = new Snep_Form_SubForm(null, $form_xml->khomp, "khomp");
