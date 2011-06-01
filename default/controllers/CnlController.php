abf1b71d0dace497e152dfab191a353c9128bd46
rbozzetti <rbozzetti@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2011-06-01 21:23:18 +0000

    revisao da cnl #Parcial

--- default/controllers/CnlController.php
+++ default/controllers/CnlController.php
@@ -21,6 +21,40 @@ class CnlController extends Zend_Controller_Action {
 
     public function indexAction() {
 
+
+        // verification procedure
+        $db = Zend_Registry::get('db');
+        $select = $db->select()
+                ->from('ars_estado');
+        $stmt = $select->query();
+        $result = $stmt->fetchAll();
+
+        // insert state data
+        if( count($result ) < 26 ) {
+            $brStates = array( 'AC'=>'Acre', 'AL'=>'Alagoas','AM'=>'Amazonas','AP'=>'Amapá',
+                    'BA'=>'Bahia','CE'=>'Ceará','DF'=>'Distrito Federal',
+                    'ES'=>'Espírito Santo','GO'=>'Goiás','MA'=>'Maranhão',
+                    'MG'=>'Minas Gerais','MS'=>'Mato Grosso do Sul',
+                    'MT'=>'Mato Grosso','PA'=>'Pará','PB'=>'Paraíba',
+                    'PE'=>'Pernambuco','PI'=>'Piauí','PR'=>'Paraná','RJ'=>'Rio de Janeiro',
+                    'RN'=>'Rio Grande do Norte','RO'=>'Rondônia','RR'=>'Roraima',
+                    'RS'=>'Rio Grande do Sul','SC'=>'Santa Catarina','SE'=>'Sergipe',
+                    'SP'=>'São Paulo', 'TO'=>'Tocantins'  );
+
+            foreach($brStates as $uf => $state) {
+                $db->beginTransaction();
+                try {
+                    $_state = array('cod' => $uf, 'name' => $state);
+                    $db->insert('ars_estado', $_state);
+                    $db->commit();
+
+                } catch (Exception $ex) {
+                    $db->rollBack();
+                    throw $ex;
+                }
+            }
+        }
+
         $this->view->breadcrumb = $this->view->translate("Configurações » Atualização CNL");
 
         $config = Zend_Registry::get('config');
@@ -34,7 +68,7 @@ class CnlController extends Zend_Controller_Action {
         $element->setLabel( $this->view->translate('Arquivo CNL') )
                 ->setDestination('/tmp/');
         
-        $element->addValidator('Extension', false, 'bz2');
+        $element->addValidator('Extension', false, array('tar.bz2', 'bz2'));
         $element->removeDecorator('DtDdWrapper');
         $form->addElement($element, 'cnl');
 
@@ -43,7 +77,6 @@ class CnlController extends Zend_Controller_Action {
 
         $form->setAttrib('enctype', 'multipart/form-data');
 
-        $this->view->form = $form;
         $this->view->valid = true;
 
         if ($this->_request->getPost()) {
@@ -56,58 +89,51 @@ class CnlController extends Zend_Controller_Action {
 
                 $adapter = new Zend_File_Transfer_Adapter_Http();
 
-                if (!$adapter->isValid()) {
-                    echo "Formato de arquivo invalido";
-                    exit;
-                } else {
-                    $adapter->receive();
-
-                    $fileName = $adapter->getFileName();
-                    exec("tar xjvf {$fileName} -C /tmp");
-
-                    $json = file_get_contents(substr($fileName,0,-8));
-                    $cnl = (Zend_Json_Decoder::decode($json, Zend_Json::TYPE_ARRAY));
-
-                    $data = $cnl["operadoras"];
-                    unset($cnl["operadoras"]);
+                if ($adapter->isValid()) {
 
                     Snep_Cnl::delPrefixo();
                     Snep_Cnl::delDDD();
                     Snep_Cnl::delCidade();
                     Snep_Cnl::delOperadora();
 
-                    foreach ($data as $operadora => $id) {
+                    $adapter->receive();
+                    $fileName = $adapter->getFileName();
 
-                        Snep_Cnl::addOperadora($id,$operadora);
+                    exec("tar xjvf {$fileName} -C /tmp");
 
+                    $json = file_get_contents(substr($fileName,0,-8));
+                    $cnl = (Zend_Json_Decoder::decode($json, Zend_Json::TYPE_ARRAY));
+
+                    $carriers = $cnl["operadoras"];
+                    unset( $cnl["operadoras"] );
+
+                    foreach ($carriers as $carrier => $idCarr) {
+                        Snep_Cnl::addOperadora($idCarr, $carrier );
                     }
 
+                    /*
                     foreach ($cnl as $data => $id ) {
-
                         foreach ($id as $estado => $es) {
-
                             foreach ($es as $ddd => $d) {
-
                                 foreach ($d as $cidade => $pre) {
-
-                                    $idCidade = Snep_Cnl::addCidade($cidade);
-                                    Snep_Cnl::addDDD($ddd,$estado,$idCidade);
-
+                                   // $idCidade = Snep_Cnl::addCidade($cidade);
+                                   // Snep_Cnl::addDDD($ddd,$estado,$idCidade);
                                     foreach ($pre as $prefixo => $op) {
-
-                                        Snep_Cnl::addPrefixo($prefixo,$idCidade,$op);
-
+                                     //   Snep_Cnl::addPrefixo($prefixo,$idCidade,$op);
                                     }
                                 }
                             }
                         }
                     }
+                     * 
+                     */
+
+                    exit;
 
                     $this->_redirect("/default/cnl/");
                 }
-                    
             }
         }
+       $this->view->form = $form;
     }
-
 }
