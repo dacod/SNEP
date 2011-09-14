ac3669a4bfefbcd4e0efb560c1156ad06823a918
lucasseid <lucasseid@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2011-09-14 19:08:09 +0000

    correções snep 1.3.7 #107

--- src/ramais_varios.php
+++ src/ramais_varios.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *  This file is part of SNEP.
  *  Para território Brasileiro leia LICENCA_BR.txt
@@ -17,47 +18,45 @@
  *  You should have received a copy of the GNU General Public License
  *  along with SNEP.  If not, see <http://www.gnu.org/licenses/>.
  */
+require_once("../includes/verifica.php");
+require_once("../configs/config.php");
+ver_permissao(17);
 
- require_once("../includes/verifica.php");  
- require_once("../configs/config.php");
- ver_permissao(17) ;
 
-
- // Testa conexao com Asterisk
+// Testa conexao com Asterisk
 try {
     $asterisk = PBX_Asterisk_AMI::getInstance();
     $asterisk->Command("core show version");
-}
-catch( Asterisk_Exception_CantConnect $ex ) {
+} catch (Asterisk_Exception_CantConnect $ex) {
     display_error("Falha ao conectar com o servidor Asterisk: {$ex->getMessage()}", true, -1);
 }
- 
- // Monta Lista de Grupos de Captura
- if (!isset($grupos) || count($grupos) == 0) {
+
+// Monta Lista de Grupos de Captura
+if (!isset($grupos) || count($grupos) == 0) {
     try {
-       $sql_grp = "SELECT * FROM grupos ORDER by nome" ;
-       $row_grp = $db->query($sql_grp)->fetchAll();
+        $sql_grp = "SELECT * FROM grupos ORDER by nome";
+        $row_grp = $db->query($sql_grp)->fetchAll();
     } catch (Exception $e) {
-       display_error($LANG['error'].$e->getMessage(),true) ;
+        display_error($LANG['error'] . $e->getMessage(), true);
     }
     unset($val);
-    $grupos = array(""=>$LANG['undef']); 
-    foreach ($row_grp as $val) 
-       $grupos[$val['cod_grupo']] = $val['nome'] ;
-    asort($grupos) ;  
- }
-
- // Monta Lista de Grupos de Ramais
- $user_groups = array() ;
- try {
-   $sql_grp = "SELECT * FROM groups WHERE name != 'all' ORDER BY name" ;
-   $row_grp = $db->query($sql_grp)->fetchAll();
-} catch (Exception $e) {
-   display_error($LANG['error'].$e->getMessage(),true) ;
+    $grupos = array("" => $LANG['undef']);
+    foreach ($row_grp as $val)
+        $grupos[$val['cod_grupo']] = $val['nome'];
+    asort($grupos);
 }
 
-foreach($row_grp as $grp) {
-    switch($grp['name']) {
+// Monta Lista de Grupos de Ramais
+$user_groups = array();
+try {
+    $sql_grp = "SELECT * FROM groups WHERE name != 'all' ORDER BY name";
+    $row_grp = $db->query($sql_grp)->fetchAll();
+} catch (Exception $e) {
+    display_error($LANG['error'] . $e->getMessage(), true);
+}
+
+foreach ($row_grp as $grp) {
+    switch ($grp['name']) {
         case 'admin':
             $grp_name = 'Administradores';
             break;
@@ -83,7 +82,6 @@ $canais = array(
 $trunks = array();
 foreach (PBX_Trunks::getAll() as $tronco) {
     $trunks[$tronco->getId()] = $tronco->getId() . " - " . $tronco->getName();
-
 }
 $smarty->assign('TRUNKS', $trunks);
 
@@ -92,153 +90,164 @@ $khompInfo = new PBX_Khomp_Info();
 
 
 $fxs_list = array();
-if( $khompInfo->hasWorkingBoards() ) {
+if ($khompInfo->hasWorkingBoards()) {
     $khomp_boards = $khompInfo->boardInfo();
     foreach ($khomp_boards as $board) {
-        if( preg_match("/FXS/", $board['model']) ) {
+        if (preg_match("/FXS/", $board['model'])) {
             $fxs_list[] = $board;
         }
     }
 }
 
-$smarty->assign('FXSS',$fxs_list);
-   
- // Variaveis de ambiente do form
- $smarty->assign('ACAO',$acao);
- $smarty->assign('PROTOTYPE',true);
- $smarty->assign('OPCOES_YN',$tipos_yn) ;
- $smarty->assign('OPCOES_DTMF',$tipos_dtmf) ;
- $smarty->assign('OPCOES_CODECS',$tipos_codecs) ;
- $smarty->assign('OPCOES_GRUPOS',$grupos);
- $smarty->assign('OPCOES_CANAL',$canais) ;
- $smarty->assign('OPCOES_USERGROUPS',$user_groups);
- 
- if ($acao == "cadastrar") {
+$smarty->assign('FXSS', $fxs_list);
+
+// Variaveis de ambiente do form
+$smarty->assign('ACAO', $acao);
+$smarty->assign('PROTOTYPE', true);
+$smarty->assign('OPCOES_YN', $tipos_yn);
+$smarty->assign('OPCOES_DTMF', $tipos_dtmf);
+$smarty->assign('OPCOES_CODECS', $tipos_codecs);
+$smarty->assign('OPCOES_GRUPOS', $grupos);
+$smarty->assign('OPCOES_CANAL', $canais);
+$smarty->assign('OPCOES_USERGROUPS', $user_groups);
+
+if ($acao == "cadastrar") {
     cadastrar();
- } else {
-   $titulo = $LANG['menu_register']." -> ".$LANG['menu_ramais']." -> ".$LANG['include']." ".$LANG['various'];
-   principal() ;
- }
- 
-/*------------------------------------------------------------------------------
- Funcao PRINCIPAL - Monta a tela principal da rotina
- ------------------------------------------------------------------------------*/
- function principal()  {
-   global $db,$smarty,$titulo,$codecs_default,$SETUP ;
-   // Codecs padrao
-   $row = array() ;
-   $row = $row + $codecs_default ;
+} else {
+    $titulo = $LANG['menu_register'] . " -> " . $LANG['menu_ramais'] . " -> " . $LANG['include'] . " " . $LANG['various'];
+    principal();
+}
 
-   // Authenticate
-   $row['usa_auth'] = "No" ;
+/* ------------------------------------------------------------------------------
+  Funcao PRINCIPAL - Monta a tela principal da rotina
+  ------------------------------------------------------------------------------ */
 
-   $row['group'] = "users";
-   
-   // Variavies do Template
-   $smarty->assign('dt_ramais',$row) ;
-   $smarty->assign('ACAO',"cadastrar") ;
-   display_template("ramais_varios.tpl",$smarty,$titulo) ;
-} 
-/*------------------------------------------------------------------------------
- Funcao CADASTRAR - Inclui um novo registro
-------------------------------------------------------------------------------*/
- /**
-  * Insere extensão no banco de dados.
-  *
-  * @param int $number numero do ramal
-  * @param array $khompChannels lista de canais khomp para inserção em fxs
-  */
+function principal() {
+    global $db, $smarty, $titulo, $codecs_default, $SETUP;
+    // Codecs padrao
+    $row = array();
+    $row = $row + $codecs_default;
+
+    // Authenticate
+    $row['usa_auth'] = "No";
+
+    $row['group'] = "users";
+
+    // Variavies do Template
+    $smarty->assign('dt_ramais', $row);
+    $smarty->assign('ACAO', "cadastrar");
+    display_template("ramais_varios.tpl", $smarty, $titulo);
+}
+
+/* ------------------------------------------------------------------------------
+  Funcao CADASTRAR - Inclui um novo registro
+  ------------------------------------------------------------------------------ */
+
+/**
+ * Insere extensão no banco de dados.
+ *
+ * @param int $number numero do ramal
+ * @param array $khompChannels lista de canais khomp para inserção em fxs
+ */
 function insert_exten($number, &$khompChannels) {
     global $SETUP, $LANG, $db;
-    $system_range = explode("-", $SETUP['canais']['peers_range']);
-    if( $number < (int) $system_range[0] || $number > (int) $system_range[1] ) {
-        throw new Exception($LANG['msg_channeloutinterval']);
-    }
-    else {
-        $pickupGroup = $_POST['pickupgroup'] != "" ? $_POST['pickupgroup'] : null;
-        $group       = $_POST['group'];
-        
-        $options = array(
-            // Opções gerais para qualquer tecnologia.
-            "name" => $number,
-            "username" => $number,
-            "fromuser" => $number,
-            "mailbox" => $number,
-            "secret" => "$number$number",
-            "callerid" => "Ramal $number <$number>",
-            "context" => "default",
-            "type" => "peer",
-            "peer_type" => "R",
-            "authenticate" => 0,
-            "group" => $group,
-            "pickupgroup" => $pickupGroup,
-            "host" => "dynamic",
-            "disallow" => "all",
-            // Opções padrões para tecnologias não IP
-            "nat" => "no",
-            "qualify" => "no",
-            "dtmfmode" => "rfc2833",
-            "allow" => "ulaw;alaw;gsm;g729;"
-        );
 
-        $tech = $_POST['tech'];
 
-        if( $tech == 'VIRTUAL' ) {
-            $trunk = $_POST['trunk'];
-            $options["canal"] = "VIRTUAL/$trunk";
-        }
-        else if( $tech == 'KHOMP' ) {
-            $options['canal'] = array_pop($khompChannels);
-        }
-        else {
-            $options['canal'] = "$tech/$number";
+    $ranges = explode(";", $SETUP['canais']['peers_range']);
+    
+    $error = false;
+
+
+    foreach ($ranges as $key => $range) {
+        $system_range = explode("-", $range);
+        if ($number < (int) $system_range[0] || $number > (int) $system_range[1]) {
+            $error = true;
+        } else {
             
-            if(isset($_POST['nat'])) {
-                $options["nat"] = $_POST['nat'];
-            }
-            if(isset($_POST['qualify'])) {
-                $options["qualify"] = $_POST['qualify'];
-            }
-            if(isset($_POST['dtmfmode'])) {
-                $options["dtmfmode"] = $_POST['dtmfmode'];
+            $error = false;
+            $pickupGroup = $_POST['pickupgroup'] != "" ? $_POST['pickupgroup'] : null;
+            $group = $_POST['group'];
+
+            $options = array(
+                // Opções gerais para qualquer tecnologia.
+                "name" => $number,
+                "username" => $number,
+                "fromuser" => $number,
+                "mailbox" => $number,
+                "secret" => "$number$number",
+                "callerid" => "Ramal $number <$number>",
+                "context" => "default",
+                "type" => "peer",
+                "peer_type" => "R",
+                "authenticate" => 0,
+                "group" => $group,
+                "pickupgroup" => $pickupGroup,
+                "host" => "dynamic",
+                "disallow" => "all",
+                // Opções padrões para tecnologias não IP
+                "nat" => "no",
+                "qualify" => "no",
+                "dtmfmode" => "rfc2833",
+                "allow" => "ulaw;alaw;gsm;g729;"
+            );
+
+            $tech = $_POST['tech'];
+
+            if ($tech == 'VIRTUAL') {
+                $trunk = $_POST['trunk'];
+                $options["canal"] = "VIRTUAL/$trunk";
+            } else if ($tech == 'KHOMP') {
+                $options['canal'] = array_pop($khompChannels);
+            } else {
+                $options['canal'] = "$tech/$number";
+
+                if (isset($_POST['nat'])) {
+                    $options["nat"] = $_POST['nat'];
+                }
+                if (isset($_POST['qualify'])) {
+                    $options["qualify"] = $_POST['qualify'];
+                }
+                if (isset($_POST['dtmfmode'])) {
+                    $options["dtmfmode"] = $_POST['dtmfmode'];
+                }
+
+                $allow = "";
+                $allow .= ( strlen(trim($_POST['cod1'])) > 0) ? $_POST['cod1'] : "";
+                $allow .= ( strlen(trim($_POST['cod2'])) > 0) ? ";{$_POST['cod2']}" : ";";
+                $allow .= ( strlen(trim($_POST['cod3'])) > 0) ? ";{$_POST['cod3']}" : ";";
+                $allow .= ( strlen(trim($_POST['cod4'])) > 0) ? ";{$_POST['cod4']}" : ";";
+                $allow .= ( strlen(trim($_POST['cod5'])) > 0) ? ";{$_POST['cod5']}" : ";";
+
+                $options['allow'] = $allow;
             }
 
-            $allow="" ;
-            $allow .= (strlen(trim($_POST['cod1']))>0) ? $_POST['cod1'] : "" ;
-            $allow .= (strlen(trim($_POST['cod2']))>0) ? ";{$_POST['cod2']}" : ";" ;
-            $allow .= (strlen(trim($_POST['cod3']))>0) ? ";{$_POST['cod3']}" : ";" ;
-            $allow .= (strlen(trim($_POST['cod4']))>0) ? ";{$_POST['cod4']}" : ";" ;
-            $allow .= (strlen(trim($_POST['cod5']))>0) ? ";{$_POST['cod5']}" : ";" ;
 
-            $options['allow'] = $allow;
+            $fields = "";
+            $values = "";
+            foreach ($options as $field => $value) {
+                $fields .= ",`$field`";
+                if ($value === null) {
+                    $values .= ",NULL";
+                } else {
+                    $values .= ",'$value'";
+                }
+            }
+
+            $fields = trim($fields, ',');
+            $values = trim($values, ',');
+
+            $sql = "INSERT INTO peers($fields) VALUES($values);";
+
+            $db->exec($sql);
         }
-
-
-        $fields = "";
-        $values = "";
-        foreach ($options as $field => $value) {
-            $fields .= ",`$field`";
-            if($value === null) {
-                $values .= ",NULL";
-            }
-            else {
-                $values .= ",'$value'";
-            }
-        }
-
-        $fields = trim($fields, ',');
-        $values = trim($values, ',');
-
-        $sql = "INSERT INTO peers($fields) VALUES($values);";
-
-        $db->exec($sql);
-
-
-
+    }
+    
+    if ($error){
+        throw new Exception($LANG['msg_channeloutinterval']);
     }
 }
 
-function cadastrar()  {
+function cadastrar() {
     global $SETUP, $LANG, $db;
 
     // Contando ramais para serem incluídos.
@@ -246,11 +255,11 @@ function cadastrar()  {
 
     $khompInfo = new PBX_Khomp_Info();
     $khompChannels = array();
-    
-    if($_POST['tech'] == "KHOMP") {
+
+    if ($_POST['tech'] == "KHOMP") {
         foreach ($_POST['fxs'] as $id => $trash) {
             $boardInfo = $khompInfo->boardInfo($id);
-            for( $i = 0; $i < $boardInfo['channels']; $i++ ) {
+            for ($i = 0; $i < $boardInfo['channels']; $i++) {
                 $khompChannels[] = "KHOMP/b{$boardInfo['id']}c$i";
             }
         }
@@ -260,7 +269,7 @@ function cadastrar()  {
         $khompPeers = $db->query($sql)->fetchAll();
 
         foreach ($khompPeers as $khompPeer) {
-            if( in_array($khompPeer['canal'], $khompChannels) ) {
+            if (in_array($khompPeer['canal'], $khompChannels)) {
                 unset($khompChannels[array_search($khompPeer['canal'], $khompChannels)]);
             }
         }
@@ -271,33 +280,29 @@ function cadastrar()  {
     $db->beginTransaction();
     try {
         foreach ($range as $number) {
-            if( is_numeric($number)) {
+            if (is_numeric($number)) {
                 try {
                     PBX_Usuarios::get($number);
-                }
-                catch( PBX_Exception_NotFound $ex ) {
+                } catch (PBX_Exception_NotFound $ex) {
                     insert_exten($number, $khompChannels);
                 }
 
                 Snep_Vinculos::setVinculos($number, 'R', $number);
-            }
-            else {
-                $number = explode("-",$number);
+            } else {
+                $number = explode("-", $number);
                 foreach (range($number[0], $number[1]) as $number) {
                     try {
                         PBX_Usuarios::get($number);
-                    }
-                    catch( PBX_Exception_NotFound $ex ) {
+                    } catch (PBX_Exception_NotFound $ex) {
                         insert_exten($number, $khompChannels);
                     }
-                    
+
                     Snep_Vinculos::setVinculos($number, 'R', $number);
                 }
             }
         }
         $db->commit();
-    }
-    catch(Exception $ex) {
+    } catch (Exception $ex) {
         $db->rollBack();
         display_error($ex->getMessage(), true);
         exit();
@@ -306,8 +311,8 @@ function cadastrar()  {
 
     // Gravando mudanças no arquivo e recarregando configurações no asterisk.
     grava_conf();
-    ast_status("sip reload","");
-    ast_status("iax2 reload","");
+    ast_status("sip reload", "");
+    ast_status("iax2 reload", "");
 
     echo "<meta http-equiv='refresh' content='0;url=../src/extensions.php'>\n";
 }
