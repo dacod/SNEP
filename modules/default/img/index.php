4282c96edf9f38f44a4efa1101af86347a86676a
rbozzetti <rbozzetti@ec8f2f6f-633d-0410-af8e-babccdf4900d>
2011-12-16 02:07:35 +0000

    Imagens e css para dentro do modulo default

---
+++ modules/default/img/index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * index.php
+ *
+ * Copyright (c) 2002 The SquirrelMail Project Team 
+ * Adaptado ao Projeto tarefas pela Opens Tecnologia - 2002
+ * Licensed under the GNU GPL. For full terms see the file COPYING.
+ *
+ * Este arquivo simplesmente pega qualquer tentativa de ver o codigo fonte
+ * e envia a essas pessoas a janela de login. Ate este ponto, nenhuma
+ * tentativa é feita para ver se a pessoa esta logada ou não.
+ *
+ * Tradução: Flavio H Somensi em 13/11/2002. 
+*/
+header("Location:../index.php");
+?>
